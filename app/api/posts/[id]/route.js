b5f8516876f8581e2b9f9a5e1e45b71f614cbca6
Alexandros Dianellos <153101832+AlexisDianellos@users.noreply.github.com>
2024-09-14 15:18:07 +0300

    Update route.js

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -1,5 +1,9 @@
 import connectToDatabase from '@/lib/mongodb';
 import Post from '@/models/Post';
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '@/lib/authOptions';
+import { del,put } from '@vercel/blob';
 
 export async function GET(req, { params }) {
   await connectToDatabase();
@@ -8,15 +12,100 @@ export async function GET(req, { params }) {
   return new Response(JSON.stringify(post), { status: 200 });
 }
 
-export async function PUT(req, { params }) {
-  await connectToDatabase();
-  const data = await req.json();
-  const updatedPost = await Post.findByIdAndUpdate(params.id, data, { new: true });
-  return new Response(JSON.stringify(updatedPost), { status: 200 });
+export async function PATCH(req, { params }) {
+  
+  try{
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+    }
+    
+    await connectToDatabase();
+
+    const { searchParams } = new URL(req.url);
+    const filename = searchParams.get('filename') || "";
+
+    if(filename){
+
+      const postId = params.id;
+
+      const formData = await req.formData();
+      const title = formData.get('title');
+      const content = formData.get('content');
+      const file = formData.get('file'); 
+
+      if(!file){
+        return NextResponse.json({ error: 'Error, no image url' }, { status: 400 });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (!post) {
+        return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+      }
+
+      if (post.createdBy.toString() !== session.user.id) {
+        return NextResponse.json({ error: 'You do not have permission to edit this post' }, { status: 403 });
+      }
+
+      await del(post.image);
+
+      const blob = await put(filename, file, {
+        access: 'public',
+      });
+
+      const image=blob.url;
+
+      const updatedPost = await Post.findByIdAndUpdate(postId, {title,content,image}, { new: true });
+
+      return NextResponse.json(updatedPost, { status: 200 });
+  }else{
+    return NextResponse.json({message:"No filename detected"});
+  }
+
+  }catch(err){
+    console.error("Error editing post ",err);
+    return NextResponse.json({ error: 'Failed to edit post' }, { status: 500 });
+  }
+  
 }
 
 export async function DELETE(req, { params }) {
-  await connectToDatabase();
-  await Post.findByIdAndDelete(params.id);
-  return new Response(null, { status: 204 });
+  try{
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+    }
+    
+    await connectToDatabase();
+
+    const postId = params.id;
+    const formData = await req.formData(); // Use formData to handle file uploads
+    const url = formData.get('image')||"";
+    console.log(url)
+    if(!url){
+      return NextResponse.json({ error: 'Error, no image url' }, { status: 400 });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+    }
+
+    if (post.createdBy.toString() !== session.user.id) {
+      return NextResponse.json({ error: 'You do not have permission to delete this post' }, { status: 403 });
+    }
+
+    await post.deleteOne();
+    await del(url);
+
+    return NextResponse.json({ success: 'Post deleted' }, { status: 200 });
+
+  }catch(err){
+    console.error("Error deleting post ",err);
+    return NextResponse.json({ error: 'Failed to delete post' }, { status: 500 });
+  }
 }
